2fff44eec30ac49adaad523542836900018d7533
liannaccone <lciannaccone@gmail.com>
2018-01-28 17:48:13 -0800

    adding js functions to remove start button and show questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+console.log("hi");
 $(document).ready(function() {
 
 
@@ -8,26 +10,26 @@ $(document).ready(function() {
 		
 		// question 1; index[0]
 		{
-			question: "do you like cats";
-			possibleAnswers: ["yes", "maybe", "no", "absolutely not"];
-			correctAnswer: "absolutely not";
-			correctImage: ""
+			question: "do you like cats",
+			possibleAnswers: ["yes", "maybe", "no", "absolutely not"],
+			correctAnswer: "absolutely not",
+			correctImage: "",
 		},
 
 		// question 2; index[1]
 		{
-			question: ""
-			possibleAnswers: []		
-			correctAnswer: ""
-			correctImage: ""
+			question: "",
+			possibleAnswers: [],
+			correctAnswer: "",
+			correctImage: "",
 		},
 
 		// question 3; index[2]
 		{
-			question: ""
-			possibleAnswers: []		
-			correctAnswer: ""
-			correctImage: ""
+			question: "",
+			possibleAnswers: [],	
+			correctAnswer: "",
+			correctImage: "",
 		}
 
 	] //closing out questionsArr
@@ -51,7 +53,21 @@ $(document).ready(function() {
 //  ====================
 
 	
-	//  adds start button 
+	//  removes start button
+	function removeStartButton() {
+		$("#startButton").remove();
+	};
+
+
+	// populates fields with the first question
+	function showQuestion() {
+		$("#question").html(questionsArr[count].question)
+		$("#answers").html("<li>" + questionsArr[count].possibleAnswers[0] + 
+			"</li><li>" + questionsArr[count].possibleAnswers[1] + 
+			"</li><li>" + questionsArr[count].possibleAnswers[2] + 
+			"</li><li>" + questionsArr[count].possibleAnswers[3] + 
+			"</li>");
+	};
 
 
 
