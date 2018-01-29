cfdc8b998aefd87f09c14cf9542dedcf523de39d
liannaccone <lciannaccone@gmail.com>
2018-01-28 17:10:33 -0800

    adding initial html structure and js variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+
+// VARIABLES
+
+	// array of questions
+	var questionsArr = [
+		
+		// question 1; index[0]
+		{
+			question: "do you like cats";
+			possibleAnswers: ["yes", "maybe", "no", "absolutely not"];
+			correctAnswer: "absolutely not";
+			correctImage: ""
+		},
+
+		// question 2; index[1]
+		{
+			question: ""
+			possibleAnswers: []		
+			correctAnswer: ""
+			correctImage: ""
+		},
+
+		// question 3; index[2]
+		{
+			question: ""
+			possibleAnswers: []		
+			correctAnswer: ""
+			correctImage: ""
+		}
+
+	] //closing out questionsArr
+
+
+
+
+	// count will keep track of the index of the currently displaying question
+	var count = 0
+
+
+	//  variables to keep track of use responses
+	var correctCount = 0
+	var wrongCount = 0
+	var unansweredCound = 0
+
+
+
+
+//  FUNCTIONS
+//  ====================
+
+	
+	//  adds start button 
+
+
+
+
+// Main Process
+// ======================
+
+
+
+
+	// on user click of start button, populate
+
+
+
+
+}); // closing out document ready
